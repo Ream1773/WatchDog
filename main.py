47b9f30b208167b458a34c8c42fbe2eb17578506
Ream1773 <91226749+Ream1773@users.noreply.github.com>
2022-11-06 02:06:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,54 @@
 import os
+import Logger
+
+""" ::Call logger:: """
+logg = Logger.get_logger()
 
 
 class WatchD:
-    def __init__(self, path):
-        self.path = path
-        self.og_file_stamp = 0
-        self.og_dir_stamp = 0
+    """
+    Main class that scans files recursively of provided path
+    and logs changes made to them.
+    """
+    def __init__(self):
+        self.og_file_stamp = int()
+        self.og_dir_stamp = int()
 
-    def get_tree(self):
+    def get_tree(self, path: str):
         while True:
-            for root, dirs, files in os.walk(self.path, topdown=True):
+            for root, dirs, files in os.walk(path, topdown=True):
                 for name in files:
                     file_key = os.path.join(root, name)
                     file_stamp = os.stat(file_key).st_mtime
-                    # print(file_key + "::", file_stamp)
-                    if file_stamp != self.og_file_stamp:
-                        print("{0} was modified at {1}".format(file_key, file_stamp))
+                    self.og_file_stamp = file_stamp
+                    # print(self.og_file_stamp, file_stamp)
+                    if self.og_file_stamp != file_stamp:
                         self.og_file_stamp = file_stamp
-                    else:
-                        return
+                        logg.info("{0} was modified at {1}".format(file_key, file_stamp))
                 for name in dirs:
                     dir_key = os.path.join(root, name)
                     dir_stamp = os.stat(dir_key).st_mtime
-                    if dir_stamp != self.og_dir_stamp:
-                        print("{0} was modified at {1}".format(dir_key, dir_stamp))
+                    self.og_dir_stamp = dir_stamp
+                    # print(self.og_dir_stamp, dir_stamp)
+                    if self.og_dir_stamp != dir_stamp:
                         self.og_dir_stamp = dir_stamp
-                        return
-                    else:
-                        return
+                        logg.info("{0} was modified at {1}".format(dir_key, dir_stamp))
 
 
 def run():
-    prompt = input("Please enter the path you would like to monitor:")
-    init = WatchD(repr(prompt))
-    init.get_tree()
-    return prompt
+    while True:
+        try:
+            prompt = input("Please enter the path you would like to monitor or 'exit':")
+            if prompt != "exit":
+                os.chdir(prompt)
+                passed_path = os.getcwd()
+                init = WatchD()
+                init.get_tree(passed_path)
+            else:
+                break
+        except KeyboardInterrupt and FileNotFoundError as err:
+            print(err)
+            exit(1)
 
 
 if __name__ == "__main__":

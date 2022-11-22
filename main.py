c5cd30cf4dddc36b0068e2c47f11f32f18d450b6
Ream1773 <91226749+Ream1773@users.noreply.github.com>
2022-11-22 23:27:07 +0200

    Updated 22.11

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import os
 import Logger
-from WatchDog import WatchD
-# CR: All magic numbers and text should be declared as constants here. For example:
+from WatchDog import WatchDog
+# TODO:: Implement sys arg parser
 
 
 def main():
     """ ::Call logger, WatchDog class, User input:: """
     logg = Logger.get_logger()
     # prompt = input("Please enter the path you would like to monitor or 'exit':")
-    prompt = r"C:\Users\Ream\Desktop\test"
+    prompt = r"C:\Users\Ream Sadan\Desktop\test"
     if prompt != "exit":
         os.chdir(prompt)
         passed_path = os.getcwd()
-        init = WatchD(passed_path, logg)
+        init = WatchDog(passed_path, logg)
         init.get_tree()
     else:
-        exit()
+        exit(1)
 
 
 if __name__ == "__main__":

9f966976a0a9a0f9d66f4eb9f5cb3c519755a311
Ream1773 <reamsadan11@gmail.com>
2022-11-27 01:54:01 +0200

    Fixed main class crash bug

--- WatchDog.py
+++ WatchDog.py
@@ -1,6 +1,9 @@
+import logging
 import os
+
 # TODO:: Implement the hasher function to make the program more genuine.
-# TODO:: Fix exception that occasionally occurs when files/dirs are deleted.
+# TODO:: Add feature that shows if the directory is empty.
+# TODO:: Fix bug that doesn't show file deletions if all files in the directory are deleted.
 
 
 class WatchDog:
@@ -8,37 +11,36 @@ class WatchDog:
     Main class that scans files recursively of provided path
     and logs changes made to them.
     """
-    def __init__(self, path: str, logg):
+
+    def __init__(self, path: str, logg: logging.Logger):
         self.path = path
         self.logg = logg
         self.is_first_run = True
         self.entities = {}
 
     def get_tree(self):
-        if self.is_first_run:
-            self.entities.clear()
+        self.is_first_run = True
         while True:
-            self._get_entities(self.path)
+            self._get_entities()
             self.is_first_run = False
 
-    def _get_entities(self, path: str):
+    def _get_entities(self):
         """
         Calls the _get_timestamps class method that which then scans all the
         elements of the given path param.
-        :param path:
-        :return: None
+        :return: -> None
         """
-        for root, dirs, files in os.walk(path):
+        for root, dirs, files in os.walk(self.path):
             self._check_existence(root, files, False)
             self._check_existence(root, dirs, True)
 
     def _check_existence(self, root, walk_files, is_dir):
         """
         * Checks if entity is a file or directory.
-        * Checks if the file/dir exists and sets modification time if file/dir exists.
-        :param -> root:
-        :param -> walk_files:
-        :param -> is_dir:
+        * Checks if the file/dir exists and creates modification time if file/dir exists.
+        :param -> root: Path provided by main.py essentially
+        :param -> walk_files: iteration of all files in provided path.
+        :param -> is_dir: function to check file type(dir/file)
         :return: -> None
         """
         if is_dir:
@@ -49,18 +51,24 @@ class WatchDog:
         for name in walk_files:
             keys = os.path.join(root, name)
             try:
-                if not os.path.exists(keys):
-                    self.entities.pop(name, None)
-                    self.logg.info(f"[-]: {file_type} [-] {keys} deleted!")
-                else:
-                    mod_time = os.stat(keys).st_mtime
-                    creation_time = os.stat(keys).st_ctime
-                    self.check_changed(keys, mod_time, creation_time, file_type)
-            except FileNotFoundError as e:
-                print(f"{e}")
-                pass
+                stat = os.stat(keys)
+            except FileNotFoundError:
+                self.entities.pop(keys, None)
+                self.logg.info(f"[-]: {file_type} [-] {keys} deleted!")
+                continue
+
+            mod_time = stat.st_mtime
+            creation_time = stat.st_ctime
+            self.check_changed(keys, mod_time, creation_time, file_type)
 
     def check_changed(self, keys, mod_time, creation_time, file_type):
+        """
+        :param -> keys: File/Directory name
+        :param -> mod_time: Modification time
+        :param -> creation_time: Creation time
+        :param -> file_type: Either directory or File
+        :return:
+        """
         log_text = str()
 
         if keys in self.entities:
@@ -69,9 +77,7 @@ class WatchDog:
                 self.entities[keys] = mod_time
         else:
             self.entities[keys] = mod_time
-            log_text = f"[+]NEW {file_type} [+]: {keys} added at {creation_time}!"
-            if not os.path.exists(keys):
-                self.entities.pop(keys, None)
+            log_text = f"[+] NEW {file_type} [+]: {keys} added at {creation_time}!"
 
         if not self.is_first_run and log_text:
             self.logg.info(log_text)

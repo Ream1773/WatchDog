436e0b23b8b186a6feb7087e998d11cee23b2630
Ream1773 <91226749+Ream1773@users.noreply.github.com>
2022-12-22 23:42:24 +0200

    Update WatchDog.py

--- WatchDog.py
+++ WatchDog.py
@@ -1,8 +1,6 @@
 import logging
 import os
 import hashlib
-# TODO:: Implement the hasher function to make the program more genuine.
-# TODO:: Add feature that shows if the directory is empty?
 
 
 class WatchDog:
@@ -17,8 +15,7 @@ class WatchDog:
         self.logg = logg
         self.is_first_run = True
         self.ts_dict = dict()
-        self.hash_dict = dict()
-        self.info_ = Info(path)
+        self.hashes = dict()
 
     def get_tree(self):
         self.is_first_run = True
@@ -34,16 +31,14 @@ class WatchDog:
         """
 
         walk_files = dict()
-        old_hashes = dict()
 
         for root, dirs, files in os.walk(self.path):
             walk_files.update({os.path.join(root, name): 'Directory' for name in dirs})
             walk_files.update({os.path.join(root, name): 'File' for name in files})
-            old_hashes.update({os.path.join(root, name): self.info_.return_hash(walk_files) for name in files})
-        # self.hash_dict[files] = old_hashes.values()
-        self._check_existence(walk_files, old_hashes)
 
-    def _check_existence(self, walk_files: dict, old_hashes: dict):
+        self._check_existence(walk_files)
+
+    def _check_existence(self, walk_files: dict):
 
         """
         * Checks if item exists:
@@ -51,24 +46,18 @@ class WatchDog:
         * Creates modification time if item exists.
         """
 
-        self.hash_dict = self.info_.return_hash(walk_files)
-
         for file in list(self.ts_dict.keys()):
             if file not in walk_files.keys():
-                self.logg.info(f"{self.info_.return_type().values()}[-] {file} deleted!")   # TODO:: Fix the dict.values appearing instead of file type
+                self.logg.info(f"[-]{file} deleted!")
                 self.ts_dict.pop(file, None)
-
-        for file in list(self.hash_dict.keys()):
-            if file not in old_hashes.keys():
-                self.logg.info(f"[-]{self.info_.return_type()}[-]{file} deleted!")
-                self.hash_dict.pop(file, None)
+                self.hashes.pop(file, None)
 
         for keys, file_type in walk_files.items():
             try:
                 stat = os.stat(keys)
             except FileNotFoundError:
                 self.ts_dict.pop(keys, None)
-                self.hash_dict.pop(keys, None)  # *TEST*
+                self.hashes.pop(keys, None)
                 self.logg.info(f"[-]:{file_type}[-] {keys} deleted!")
                 continue
 
@@ -87,89 +76,34 @@ class WatchDog:
         :return:
         """
 
-        hash_list = [item for item in self.hash_dict.values()]
-        next_hash = iter(hash_list)
-
         log_text = str()
 
-        # TODO:: Check if mod time is different and if hash is also diff, if hash not different then file hasn't been modified,
-        # TODO:: if it has then previous hash is replace with new hash
-
         if keys in self.ts_dict:
-            if self.ts_dict[keys] != mod_time and self.hash_dict[keys] != hash_list:
-                log_text = f"[+]{file_type}[+]:{keys}\tMODIFIED\tINFO:\nLast Modified time: {self.ts_dict[keys]}\nHash: {self.hash_dict[keys]}\t Hash modified!"
+            if self.ts_dict[keys] != mod_time:
+                log_text = f"[+]{file_type}[+]:{keys}\tMODIFIED\tINFO:\nLast modified time: {self.ts_dict[keys]}\nModified hash: {self.return_hash(keys)}"
                 self.ts_dict[keys] = mod_time
-                self.hash_dict[keys] = hash_list
+                self.hashes[keys] = self.return_hash(keys)
         else:
             self.ts_dict[keys] = mod_time
-            self.hash_dict[keys] = hash_list
-            log_text = f"[+]{file_type}[+]: {keys} added!\nINFO:\nCreation Time:{creation_time}\nHash: {self.hash_dict.values()}"
+            log_text = f"[+]{file_type}[+]: {keys} CREATED\nINFO:\nCreation Time:{creation_time}\nHash: {self.return_hash(keys)}"
+            self.hashes[keys] = self.return_hash(keys)
 
         if not self.is_first_run and log_text:
             self.logg.info(log_text)
 
-    # TODO:: -> Check if mod time is different and if hash is also diff, if hash not diff then file hasn't been modified,
-    # TODO:: if it has then previous hash is replace with new hash
-    # def _hash_comparer(self, prev_hash, mod_time):
-    #     if len(prev_hash) != len(new_hash):
-    #         return False
-    #     for i in range(len(prev_hash)):
-    #         if prev_hash[i] != new_hash[i]:
-    #             return False
-    #     return True
-
-    # def _hash_comparer(self, prev_hash, new_hash):
-    #     if len(prev_hash) != len(new_hash):
-    #         return False
-    #     for i in range(len(prev_hash)):
-    #         if prev_hash[i] != new_hash[i]:
-    #             return False
-    #     return True
-
-class Info:
-
-    """
-    Info class that collects all relevant
-    information on the items in the given path.
-    """
-
-    def __init__(self, path):
-        self.entity_info = dict()
-        self.hashes = dict()
-        self.path = path
-
-    def return_type(self):
-
-        """
-        Necessary if first run does not have items in path.
-        :return: Returns if item is File or Directory.
-        """
-
-        for root, dirs, files in os.walk(self.path):
-            self.entity_info.update({os.path.join(root, name): 'Directory' for name in dirs})
-            self.entity_info.update({os.path.join(root, name): 'File' for name in files})
-            return self.entity_info
-
-    def return_hash(self, walk_files: dict):
-
+    def return_hash(self, filename):
         """
         :return: Returns file/directory as dictionary object with key as file/dir name and value as hash.
         """
-
-        for filename in list(walk_files.keys()):
-            if filename not in list(walk_files.keys()):
-                self.hashes.pop(filename, None)
-                continue
+        while os.path.getsize(os.path.join(self.path, filename)) != 0:
+            file_hash = hashlib.sha256()
             try:
-                if os.path.getsize(os.path.join(self.path, filename)) == 0:
-                    continue
-                file_hash = hashlib.sha256()
                 with open(os.path.join(self.path, filename), 'rb') as file:
                     for chunk in iter(lambda: file.read(4096), b""):
                         file_hash.update(chunk)
-                        self.hashes[filename] = file_hash.hexdigest()
-            except PermissionError or FileNotFoundError as e:
-                self.hashes.pop(filename, None)
+                        return file_hash.hexdigest()
+            except PermissionError or FileNotFoundError:
                 print(f"{filename} is empty or does not exist!")
                 continue
-        return self.hashes
+        else:
+            return f"No hash; File empty!"
